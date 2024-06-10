fb76e0923b0e585d52bf70c472bbfa0141d78d5a
yash dudani <yashdudani2@gmail.com>
2024-06-10 12:42:49 +0530

    Initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div className="w-[70%] m-auto mt-[10%] p-10 bg-green-100 rounded-lg">
+            <h1 className="text-7xl mt-5 font-extrabold text-green-600 mb-[5%]">
+                DO YOU WANT TO REACH US ?
+            </h1>
+            {/* <button className="rounded-md text-md bg-green-600 text-white py-2 px-5 hover:bg-green-700 duration-200">
+                REACH KRLO FRAANDS &nbsp; &#8594;
+            </button> */}
+            <h1 className="text-4xl mt-5 font-extrabold text-green-600 mb-[5%]">
+                Use this email : yashdudani2@gmail.com
+                
+            </h1>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Home from "./Home";
+import Nav from "./Nav";
+import Recipes from "./Recipes";
+
+const Layout = () => {
+    return (
+        <>
+           
+            <Home />
+            <Recipes />
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
